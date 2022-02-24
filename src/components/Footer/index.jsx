0233b5b079945f6ea92dd0e87aac603486e026bf
Y.Huang <18261195537@163.com>
2022-02-24 20:08:00 +0800

    todo list finished

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,16 +1,26 @@
 import React,{ Component } from 'react'
 import './index.css'
 export default class Footer extends Component {
+  handleCheckAll = (event) => {
+    this.props.checkAllTodo(event.target.checked);
+  }
+  handleClearAllDone = () => {
+    this.props.clearAllDone();
+  }
   render() {
+    const { todos } = this.props;
+    // 已完成的个数
+    const doneCount = todos.reduce((prev,todo) => todo.done ? prev + 1 : prev,0)
+    const total = todos.length;
     return (
       <div className='todo-footer'>
         <label>
-          <input type="checkbox" />
+          <input type="checkbox" checked={doneCount === total && total !== 0} onChange={this.handleCheckAll} />
         </label>
         <span>
-          <span>已完成0</span> / 全部2
+          <span>已完成{doneCount}</span> / 全部{total}
         </span>
-        <button className='btn btn-danger'>清除已完成任务</button>
+        <button className='btn btn-danger' onClick={this.handleClearAllDone}>清除已完成任务</button>
       </div>
     )
   }
--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -12,8 +12,20 @@ export default class Item extends Component {
       this.setState({ mouse: flag })
     }
   }
+  // 勾选、取消勾选时，item的done应该改变
+  handleCheck = (id) => {
+    return (e) => {
+      this.props.updateTodo(id,e.target.checked)
+    }
+  }
+  handleDelete = (id) => {
+    if(window.confirm('确定删除吗？')) {
+      this.props.deleteTodo(id);
+    }
+
+  }
   render() {
-    const { name,done } = this.props
+    const { name,done,id } = this.props
     const { mouse } = this.state
     return (
       <li
@@ -21,10 +33,10 @@ export default class Item extends Component {
         onMouseLeave={this.handleMouse(false)}
         onMouseEnter={this.handleMouse(true)}>
         <label>
-          <input type="checkbox" defaultChecked={done} />
+          <input type="checkbox" checked={done} onChange={this.handleCheck(id)} />
           <span>{name}</span>
         </label>
-        <button className='btn btn-danger' style={{ display: mouse ? 'block' : 'none' }}>删除</button>
+        <button className='btn btn-danger' style={{ display: mouse ? 'block' : 'none' }} onClick={() => this.handleDelete(id)}>删除</button>
       </li>
     )
   }
