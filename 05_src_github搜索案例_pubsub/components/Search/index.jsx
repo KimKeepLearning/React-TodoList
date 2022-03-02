e12094cdb3954e3ce80c01b7f47259f52ce57b67
Y.Huang <18261195537@163.com>
2022-03-02 14:31:20 +0800

    github search with pubsub

---
+++ 05_src_github搜索案例_pubsub/components/Search/index.jsx
@@ -0,0 +1,36 @@
+import React,{ Component } from 'react'
+import PubSub from 'pubsub-js';
+import axios from 'axios'
+export default class Search extends Component {
+  search = () => {
+    // 获取用户输入
+    const { keyWordElement: { value: keyWord } } = this // 连续解构赋值+重命名
+
+    PubSub.publish('atguigu',{ isFirst: false,isLoading: true })
+
+    // 发送网络请求
+    axios.get(`https://api.github.com/search/users?q=${keyWord}`).then(
+      response => {
+        PubSub.publish('atguigu',{ isLoading: false,users: response.data.items })
+      },
+      error => {
+        PubSub.publish('atguigu',{
+          isLoading: false,
+          err: error.msg
+        })
+      }
+    )
+
+  }
+  render() {
+    return (
+      <section className="jumbotron">
+        <h3 className="jumbotron-heading">搜索github用户</h3>
+        <div>
+          <input ref={c => this.keyWordElement = c} type="text" placeholder="输入关键词搜索" />&nbsp;
+          <button onClick={this.search}>搜索</button>
+        </div>
+      </section>
+    )
+  }
+}
